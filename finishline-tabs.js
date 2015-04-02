4630a823d433ef35adf0724495e9fd189bd90805
hiconversiondev <ltomchik@hiconversion.com>
2015-04-02 12:14:27 -0400

    Update finishline-tabs.js
    both tabs collapsed

--- finishline-tabs.js
+++ finishline-tabs.js
@@ -1,62 +1,61 @@
-function hiconversion_callback(arg){
-  if(arg.frame.jQuery){
+function hiconversion_callback(arg) {
+  var waitTime = 60000;
+  var then = new Date();
+  hiconversion_callback_helper_print_disable(arg.frame, then, waitTime);
+  return {
+    kind: 'ok',
+    frame: arg.frame
+  };
+}
+function hiconversion_callback_helper_print_disable(frame, then, waitTime) {
+  var here = frame.document;
+  var performedUpdate = false;
+  if (frame.jQuery) {
     var $ = arg.frame.jQuery;
-    $(document).ready(function(){      
+    $(document).ready(function () {
+                   var hicRatingLink =   $('#pdpHeader > ul.headerTop.table #BVRRRatingSummaryLinkReadID > a');
+      if (hicRatingLink.length > 0 && $('.hicReviews').size() == 0) {
+        hicRatingLink.clone().addClass("hicReviews").appendTo("#BVRRRatingSummaryLinkReadID");
+                    $('.hicReviews').click(function(event){
+                      event.preventDefault();
+                      $('#ratings').show();
+          location.href = "#ratings";
+          
+          $('button#tab2Btn').removeClass('hic_expand');
+ 	  $('button#tab2Btn').addClass('hic_collapse');
+        });
         
-$("#ratings").hide();
- $("#detail").addClass("isOn");
- $("#ratings").addClass("isOff");
- $( "#tab2" ).insertBefore( "#ratings" );
-  $( "#tab2" ).wrap( "<div class='tabWrap'></div>" );
-    
-      $('button#tab1Btn').addClass('hic_collapse');
-      $('button#tab1Btn').click(function(){
-                $('#ratings').removeClass('off');
- $('.tabWrap').removeClass('off');
-    if($("#detail").hasClass("isOn")){
-      $( "#detail" ).hide();
-       $( "#detail" ).removeClass("isOn");
-      $( "#detail" ).addClass("isOff");
-       $('button#tab1Btn').removeClass('hic_collapse');
-       $('button#tab1Btn').addClass('hic_expand');
-    } else if ($("#detail").hasClass("isOff")){
-         $( "#detail" ).removeClass("isOff");
-       $( "#detail" ).addClass("isOn");
-       $( "#detail" ).show();
-      $('button#tab1Btn').addClass('hic_collapse');
-       $('button#tab1Btn').removeClass('hic_expand');
-    }
-    
-
+           $('#ratings, #detail').hide();
+      $('#tab2').insertBefore('#ratings');
+      $('#tab2').wrap('<div class=\'tabWrap\'></div>');
+      $('.tabWrap').removeClass('off');
+      $('button#tab1Btn').addClass('hic_expand');
+      $('button#tab1Btn').click(function () {
+        $('#ratings').removeClass('off');
+        $(this).toggleClass('hic_expand');
+        $('#detail').slideToggle();
         $('.tabWrap').removeClass('off');
-        
       });
       $('button#tab2Btn').addClass('hic_expand');
-      $('button#tab2Btn').click(function(){
-          $('#detail').removeClass('off');
- $('.tabWrap').removeClass('off');
-    if($("#ratings").hasClass("isOff")){
-      $("#ratings").removeClass("isOff");
-      $("#ratings").addClass("isOn");
-    
-      $( "#ratings" ).show();
-       $('button#tab2Btn').removeClass('hic_expand');
-       $('button#tab2Btn').addClass('hic_collapse');
-    } else if($("#ratings").hasClass("isOn")) {
-       $('.tabWrap').removeClass('off');
-       $( "#ratings" ).hide();
-       $("#ratings").removeClass("isOn");
-       $("#ratings").addClass("isOff");
-      $('.tabWrap').removeClass('off');
-       $('button#tab2Btn').removeClass('hic_collapse');
-       $('button#tab2Btn').addClass('hic_expand');
-    }
-    
-        
+      $('button#tab2Btn').click(function () {
+        $('#detail').removeClass('off');
+        $(this).toggleClass('hic_expand');
+        $('#ratings').slideToggle();
+        $('.tabWrap').removeClass('off');
       });
+        performedUpdate = true;      
+      }
 
     });
-    return{kind:"ok"};
+
   }
-  return{kind:"notOk"};
+  if (!performedUpdate) {
+    var now = new Date();
+    var dif = now - then;
+    if (dif < waitTime) {
+      frame.setTimeout(function () {
+        hiconversion_callback_helper_print_disable(frame, then, waitTime);
+      }, 100);
+    }
+  }
 }
